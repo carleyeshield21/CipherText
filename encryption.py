535c46b697d754d2c8f78a16d0565bfb8dff5505
carleyeshield <carlo_yamzon@yahoo.com>
2022-08-11 18:00:58 +0800

    CipherText

---
+++ encryption.py
@@ -0,0 +1,40 @@
+userinput = input('encrypt or decrypt: ')
+alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+# print(userinput)
+def encrypt():
+    plain_text = input('Type your word: ')
+    shift_amount = int(input('Choose a number from 1 to 26:\n'))
+    # print(f'{plain_text}, {shift_amount}')
+    cipher_text = ''
+    for i in plain_text:
+        # print(i)
+        pos = alphabet.index(i)
+        newpos = pos + shift_amount
+        # print(pos)
+        # print(newpos)
+        # print(alphabet[newpos])
+        cipher_text += alphabet[newpos]
+    print(cipher_text)
+
+def decrypt():
+    # print(f'Your input is {userinput}')
+    plain_text = input('Type your word: ')
+    shift_amount = int(input('Choose a number from 1 to 26:\n'))
+    # print(f'{plain_text}, {shift_amount}')
+    cipher_text = ''
+    for i in plain_text:
+        # print(i)
+        pos = alphabet.index(i)
+        newpos = pos - shift_amount
+        # print(pos)
+        # print(newpos)
+        # print(alphabet[newpos])
+        cipher_text += alphabet[newpos]
+    print(cipher_text)
+
+if userinput == 'encrypt':
+    encrypt()
+elif userinput == 'decrypt':
+    decrypt()
+else:
+    print('Invalid Input')
